460325dd6dd3dacf5da84b161cf08f63508cedec
Anssi Laitinen <korbikoski@gmail.com>
2024-01-31 16:38:02 +0200

    configuring for hosting in github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://github.com/AnssiIlari/todolistreact', // add this property and the value is the name of your repository
+  base: '/todolistreact/', // add this property and the value is the name of your repository
   plugins: [react()],
 })
